df24d44b17c76fdaca8a7eeba3c35c6d0958c536
Rafael Cristiano <rafael-cristiano@outlook.com>
2025-11-10 12:43:33 -0300

    Update GameSeries.tsx

--- src/app/game/[slug]/components/GameSeries.tsx
+++ src/app/game/[slug]/components/GameSeries.tsx
@@ -57,6 +57,7 @@ export default function GameSeries({gameSeries}: GameSeriesProps) {
                                    
                         </div>
                     ))}
+                    
                 </div>
             </div>
         </div>
