715e859dd66abf84d1958356e274f100939a1afd
Rafael Cristiano <rafael-cristiano@outlook.com>
2025-11-18 15:19:18 -0300

    att favorite icon

--- src/app/game/[slug]/components/AddFavorite.tsx
+++ src/app/game/[slug]/components/AddFavorite.tsx
@@ -1,8 +1,7 @@
 "use client"
-import { supabase } from "@/api/supabaseClient";
 import { useGame } from "@/app/hooks/useGame";
-import { Heart, HeartCrack } from "lucide-react";
-import { useEffect, useState } from "react";
+import { Heart, HeartOff } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion"
 
 interface AddFavoriteProps {
     gameId: number
@@ -28,15 +27,34 @@ export default function AddFavorite({ gameId, gameName, gameSlug, gameImage, gam
 
 
     return (
-        <div className="w-fit h-6 py-3 bg-red-600  flex items-center justify-center rounded-2xl">
-            <button onClick={handleFavorite} className="w-full flex items-center justify-center gap-1 px-1 py-0 md:px-2 md:py-2 cursor-pointer">
-                <span className="hidden group-hover:block transition-all font-semibold italic text-xs">
+        <div className="w-fit px-2 py-1 bg-red-600 flex items-center justify-center rounded-2xl">
+            <button
+                onClick={handleFavorite}
+                className="w-full flex items-center justify-center gap-1 px-1 py-0 md:px-2 md:py-2 cursor-pointer group"
+            >
+                {/* <span className="hidden group-hover:block transition-all font-semibold italic text-xs">
                     {favorite ? "Remove Favorite" : "Add to favorites"}
-                </span>
-                    {favorite 
-                        ? <HeartCrack strokeWidth={2} color="#FFFFFF" className="w-[16px] h-[16px]"/>
-                        : <Heart strokeWidth={2} color="#FFFFFF" className="w-[16px] h-[16px]"/>
-                    }
+                </span> */}
+
+                <AnimatePresence mode="wait" initial={false}>
+                    <motion.span
+                        title={favorite ? "Remove Favorite" : "Add to Favorites"}
+                        key={favorite ? "fav" : "not-fav"}
+                        initial={{ scale: 0.5, opacity: 0 }}
+                        animate={{ scale: 1.2, opacity: 1 }}
+                        exit={{ scale: 0.5, opacity: 0 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 300,
+                            damping: 15
+                        }}
+                    >
+                        {favorite 
+                            ? <HeartOff strokeWidth={2} color="#FFFFFF" size={24} />
+                            : <Heart strokeWidth={2} color="#FFFFFF" size={24} />
+                        }
+                    </motion.span>
+                </AnimatePresence>
             </button>
         </div>
     )
